d8350c940540cc82eed38ca5a3f41379a55dbfaa
skerrey <sethkerrey@gmail.com>
2022-01-22 11:30:41 -0600

    Create main-course.js

---
+++ week-3/main-course.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title: Assignment 3.2 - Restaurant App
+; File Name: main-course.js
+; Author: Professor Krasso
+; Date: 23 January 2022
+; Modified By: Seth Kerrey
+; Description: 
+;   Script for the restaurant.html file
+; Resources: 
+;   JavaScript code was taken from "WEB 330 Assign_3.pdf" 
+;   	Functions, parameters, classes, constructors 
+;		variable names and additional JavaScript 
+;       instructions provided by Prof Krasso.
+;   MainCourse provided by Prof Krasso.
+===========================================
+*/
+
+import { Product } from "./product.js"; // Import product class
+
+export class MainCourse extends Product // Export MainCourse class
+{
+    constructor(name, price)
+    {
+        super(name, price);
+    }
+}
