cc30b73f708e7d45a2a43998a93d8f0507af745f
skerrey <sethkerrey@gmail.com>
2022-01-22 16:27:23 -0600

    Update index.js

--- week-3/index.js
+++ week-3/index.js
@@ -21,5 +21,5 @@ export * from "./appetizer.js";
 export * from "./beverage.js";
 export * from "./dessert.js";
 export * from "./main-course.js";
-export * from "./bil.js";
+export * from "./bill.js";
 
