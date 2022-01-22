f618a8896c89e9823248a068f1dcecf4d0069369
skerrey <sethkerrey@gmail.com>
2022-01-22 11:30:40 -0600

    Create index.js

---
+++ week-3/index.js
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title: Assignment 3.2 - Restaurant App
+; File Name: index.js
+; Author: Professor Krasso
+; Date: 23 January 2022
+; Modified By: Seth Kerrey
+; Description: 
+;   Script for the restaurant.html file
+; Resources: 
+;   JavaScript code was taken from "WEB 330 Assign_3.pdf" 
+;   	Functions, parameters, classes, constructors 
+;		variable names and additional JavaScript 
+;       instructions provided by Prof Krasso.
+;   Export classes code provided by Prof Krasso.
+===========================================
+*/
+
+// Export classes
+export * from "./appetizer.js";
+export * from "./beverage.js";
+export * from "./dessert.js";
+export * from "./main-course.js";
+export * from "./bil.js";
+
