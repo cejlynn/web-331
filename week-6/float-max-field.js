a3844bc27fa5b110b8a86db08af566a7ef886a06
skerrey <sethkerrey@gmail.com>
2022-02-08 16:10:51 -0600

    Create float-max-field.js

---
+++ week-6/float-max-field.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title: Assignment 6.2 - Future Value App
+; File Name: float-max-field.js
+; Author: Professor Krasso
+; Date: 13 February 2022
+; Modified By: Seth Kerrey
+; Description: 
+;   Script for the kerrey-future-value.html.
+; Resources: 
+;   JavaScript code was taken from "WEB 330 Assign_6.pdf" 
+;   	Functions, parameters, classes, constructors 
+;		variable names and additional JavaScript 
+;       instructions provided by Prof Krasso.
+===========================================
+*/
