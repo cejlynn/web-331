7088e5169d04838389552502b080b3a1d839c3d1
skerrey <sethkerrey@gmail.com>
2022-02-12 17:50:10 -0600

    Update float-min-field.js

--- week-6/float-min-field.js
+++ week-6/float-min-field.js
@@ -12,5 +12,27 @@
 ;   	Functions, parameters, classes, constructors 
 ;		variable names and additional JavaScript 
 ;       instructions provided by Prof Krasso.
+;   MDN
+;       parseFloat - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat
 ===========================================
 */
+
+export class FloatMinField { // Export class FloatMinField to validator.js
+    constructor(name, field, min) {
+        this.name = name;
+        this.field = field;
+        this.min = min;
+    }
+
+    validate() { // Validate if field is greater than min, return Boolean
+        if (parseFloat(this.field) > this.min) { 
+            return true;
+        } else {
+            return false;
+            }
+        }
+
+    getMessage() { // Output message
+        return this.name + " must be more than " + this.min + ". You entered " + this.field;
+    }
+}
