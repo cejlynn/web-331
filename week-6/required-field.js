0f5f423044a6ad73cd26788c8cadbfaf7c40e319
skerrey <sethkerrey@gmail.com>
2022-02-12 17:50:21 -0600

    Update required-field.js

--- week-6/required-field.js
+++ week-6/required-field.js
@@ -12,5 +12,23 @@
 ;   	Functions, parameters, classes, constructors 
 ;		variable names and additional JavaScript 
 ;       instructions provided by Prof Krasso.
+;   Check variable as string code used
+;       https://stackoverflow.com/questions/4059147/check-if-a-variable-is-a-string-in-javascript
 ===========================================
 */
+
+export class RequiredField { // Export class RequiredField to validator.js
+    constructor(name, field) { // params
+        this.name = name;
+        this.field = field;
+    }
+
+    validate() { // Validate if string && return boolean
+        if (typeof this.field === "string" || this.field instanceof String) {
+            return Boolean(this.field);
+        }
+    } 
+    getMessage() { // Return string
+        return this.name + "is a required field.";
+    }
+}
