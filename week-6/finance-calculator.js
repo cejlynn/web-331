cf91d2fcc4e6c2493507ae8b786c0ff83107635a
skerrey <sethkerrey@gmail.com>
2022-02-12 17:50:03 -0600

    Update finance-calculator.js

--- week-6/finance-calculator.js
+++ week-6/finance-calculator.js
@@ -12,5 +12,35 @@
 ;   	Functions, parameters, classes, constructors 
 ;		variable names and additional JavaScript 
 ;       instructions provided by Prof Krasso.
+;   Code provided from Prof Krasso:
+;    1) Math.pow(interestRate, months))
+;    2) let currencyFormatter = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        });
+
+        return currencyFormatter.format(field);
+;
 ===========================================
 */
+
+export class FinanceCalculator { // Export FinanceCalculator class to kerrey-future-value.html
+    static MONTHS_IN_YEAR = 12;
+
+    static calculateFutureValue(monthlyPayment, rate, years) { // Future value calculations
+        let months = years * this.MONTHS_IN_YEAR; // Assign param
+        let interestRate = 1 + rate / 100;
+        let presentValue = monthlyPayment * months;
+        let futureValue = presentValue * (Math.pow(interestRate, months));
+        return futureValue.toFixed(2);
+    }
+
+    static convertToCurrency(field) { // USD converter
+        let currencyFormatter = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        });
+
+        return currencyFormatter.format(field); // Return currency to USD
+    }
+}
