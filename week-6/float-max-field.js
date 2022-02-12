3906e932850513a7358f2f01f363ee32a0276062
skerrey <sethkerrey@gmail.com>
2022-02-12 17:50:08 -0600

    Update float-max-field.js

--- week-6/float-max-field.js
+++ week-6/float-max-field.js
@@ -14,3 +14,23 @@
 ;       instructions provided by Prof Krasso.
 ===========================================
 */
+
+export class FloatMaxField { // Export class FloatMaxField to validator.js
+      constructor(name, field, max) {
+          this.name = name;
+          this.field = field;
+          this.max = max;
+      }
+
+      validate() { // Validate if field is less than max, return Boolean
+        if (parseFloat(this.field) < this.max) {
+              return true;
+          } else {
+              return false;
+          }
+      }
+
+      getMessage() { // Output message
+          return this.name + " must be less than " + this.max + ". You entered " + this.field;
+      }
+}
